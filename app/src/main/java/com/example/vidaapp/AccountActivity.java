bfe7baf8a86b48063f268765d057049d218ec41d
EdwardJanR <ejrobledop@gmail.com>
2023-10-29 17:50:54 -0500

    Creacion vistas servicios

--- app/src/main/java/com/example/vidaapp/AccountActivity.java
+++ app/src/main/java/com/example/vidaapp/AccountActivity.java
@@ -2,24 +2,45 @@ package com.example.vidaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class AccountActivity extends AppCompatActivity {
 
-    ImageButton ib4;
+    Button btnEnergy, btnWater, btnGas;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
-        ib4 = findViewById(R.id.imageButton4);
 
-        ib4.setOnClickListener(new View.OnClickListener() {
+        btnEnergy = findViewById(R.id.btnEnergy);
+        btnWater = findViewById(R.id.btnWater);
+        btnGas = findViewById(R.id.btnGas);
+
+        btnEnergy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i =new Intent(AccountActivity.this, MainActivity.class);
+                Intent i =new Intent(AccountActivity.this, EnergyActivity.class);
+                startActivity(i);
+            }
+        });
+        btnWater.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i =new Intent(AccountActivity.this, WaterActivity.class);
+                startActivity(i);
+            }
+        });
+        btnGas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i =new Intent(AccountActivity.this, GasActivity.class);
                 startActivity(i);
             }
         });
--- app/src/main/java/com/example/vidaapp/VistaAngie.java
+++
@@ -1,4 +0,0 @@
-package com.example.vidaapp;
-
-public class VistaAngie {
-}
